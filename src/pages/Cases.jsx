9d7e2d01eb20e9a7668fa12eb314e1c627c077af
el3raky <kameladel339@gmail.com>
2024-01-24 22:04:39 +0200

    started cases page

---
+++ src/pages/Cases.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./css/Cases.css"
+
+function Cases() {
+
+    return (
+        <>
+            <div className="cases-page">
+                <div className="container">
+                    <div className="row">
+                        <div className="sidebar-filters">
+                            
+                        </div>
+                        <div className="cases-list">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Cases
