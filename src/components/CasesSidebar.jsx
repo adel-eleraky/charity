d3d44a7225ca6be63814d1c65d54bc46cb44a0ad
el3raky <kameladel339@gmail.com>
2024-01-30 22:54:50 +0200

    added sidebar for cases page

---
+++ src/components/CasesSidebar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./css/CasesSidebar.css"
+import ReactStars from 'react-rating-stars-component';
+
+
+function CasesSidebar() {
+    return (
+        <>
+            <div className="sidebar">
+                <div className="sidebar-filter text-white rounded p-3 mb-3">
+                    <h5 className='mb-4'>Filter By</h5>
+                    <div className="price py-3 ms-2 mb-3 border-top border-3">
+                        <h6 className='mb-3'>الاموال المطلوبة</h6>
+                        <div className="inputs d-flex justify-content-between gap-15">
+                            <input type="number" className="form-control" placeholder="From" />
+                            <input type="number" className="form-control" placeholder="To" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CasesSidebar
