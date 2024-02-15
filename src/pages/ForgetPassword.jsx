7aa4343e852fdbeaa0d2acaca842e596a631e519
el3raky <kameladel339@gmail.com>
2024-02-15 02:04:50 +0200

    added form validation

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,16 +1,40 @@
 import React, { useState } from 'react'
 import "./css/ForgetPassword.css"
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as yup from "yup"
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
 
 function ForgetPassword() {
 
     const [formStatus, setFormStatus] = useState("notSubmitted")
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        setFormStatus("submitted")
+    const dispatch = useDispatch()
+    const initialValues = {
+        email: "",
     }
 
+    const validationSchema = yup.object().shape({
+        email: yup.string().required("ادخل بريدك الالكترونى").email("ادخل بريد الكترونى"),
+    })
+
+    const submitHandler = values => {
+        console.log(values)
+        setFormStatus("submitted")
+        toast.promise(
+            // implement forget password function
+            // dispatch(forgetPassword(values.email)),
+            new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    reject()
+                }, 2000)
+            }),
+            {
+                pending: 'جاري التحقق...',
+                success: 'تم التحقق بنجاح',
+                error: 'فشل التحقق'
+            }
+        )
+    }
     return (
         <>
 
@@ -21,14 +45,40 @@ function ForgetPassword() {
                             {formStatus === "notSubmitted" ?
                                 <div className="forget-password-form p-5 rounded w-75 m-auto">
                                     <h3 className='text-center mb-4'>نسيت كلمة السر ؟</h3>
-                                    <form action="" method='post' onSubmit={handleSubmit}>
-                                        <label for="exampleFormControlInput1" className="form-label">بريدك الالكترونى</label>
-                                        <div className="input-group mb-4">
-                                            <span className="input-group-text  rounded-0 rounded-end" id="basic-addon1"><i className="fa-regular fa-envelope"></i></span>
-                                            <input type="text" className="form-control rounded-0 rounded-start" placeholder="ادخل بريدك الالكترونى" aria-label="Username" aria-describedby="basic-addon1" />
-                                        </div>
-                                        <button className="btn d-block m-auto submit-btn fw-bold">ارسل الرابط الى البريد</button>
-                                    </form>
+                                    <Formik
+                                        initialValues={initialValues}
+                                        validationSchema={validationSchema}
+                                        onSubmit={submitHandler}
+                                    >
+                                        {({ values, errors, touched }) => {
+                                            return (
+                                                <Form method="post" className="needs-validation" noValidate>
+                                                    <label htmlFor="email" className="form-label">
+                                                        بريدك الالكترونى
+                                                    </label>
+                                                    <div className="input-group has-validation mb-4">
+                                                        <span
+                                                            className="input-group-text  rounded-0 rounded-end"
+                                                            id="basic-addon1"
+                                                        >
+                                                            <i className="fa-regular fa-envelope"></i>
+                                                        </span>
+                                                        <Field
+                                                            type="email"
+                                                            id="email"
+                                                            name="email"
+                                                            className={`form-control ${touched.email && errors.email && "is-invalid"} rounded-0 rounded-start`}
+                                                            placeholder="ادخل بريدك الالكترونى"
+                                                            aria-label="Username"
+                                                            aria-describedby="basic-addon1"
+                                                        />
+                                                        <ErrorMessage name="email" component="div" className="invalid-feedback d-block fs-6" />
+                                                    </div>
+                                                    <button type='submit' className="btn d-block m-auto submit-btn fw-bold">ارسل الرابط الى البريد</button>
+                                                </Form>
+                                            )
+                                        }}
+                                    </Formik>
                                 </div>
                                 :
                                 <div className='form-submitted-content text-center p-5 rounded w-75 m-auto'>
