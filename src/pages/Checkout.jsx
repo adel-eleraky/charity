3650b3d834a1a354b8d921d15413af8363fac82a
el3raky <kameladel339@gmail.com>
2024-02-16 01:40:59 +0200

    designed checkout page

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./css/Checkout.css"
+function Checkout() {
+    return (
+        <div className="Checkout-page py-4">
+            <div className="container">
+                <h3 className='text-center text-white'>صفحة الدفع</h3>
+                <div className="row">
+                    <div className="col-12 col-md-8">
+                        <div> اسم المتبرع  </div>
+                        <div className="form-floating mb-3">
+                            <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                            <label htmlFor="floatingInput"> username </label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-4"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
