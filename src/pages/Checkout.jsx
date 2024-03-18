eac43814acf020cfa931b82723404a42e954ad67
el3raky <kameladel339@gmail.com>
2024-03-18 23:21:24 +0200

    create checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -7,6 +7,7 @@ function Checkout() {
                 <h3 className='text-center text-white'>صفحة الدفع</h3>
                 <div className="row">
                     <div className="col-12 col-md-8">
+                        {/* inputs here */}
                         <div> اسم المتبرع  </div>
                         <div className="form-floating mb-3">
                             <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
@@ -17,7 +18,9 @@ function Checkout() {
                             <label htmlFor="floatingPassword">Password</label>
                         </div>
                     </div>
-                    <div className="col-12 col-md-4"></div>
+                    <div className="col-12 col-md-4">
+                        {/* payment method here */}
+                    </div>
                 </div>
             </div>
         </div>
