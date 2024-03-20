f0a9f5cc2bca77b846c49d474367a647b9d651dd
el3raky <kameladel339@gmail.com>
2024-03-20 17:36:48 +0200

    added props to the component

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,18 +1,21 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import "./css/CategoryCard.css"
 
-function CategoryCard() {
+function CategoryCard({ img, title, description, borderStyle }) {
     return (
         <>
-            <div className="category-card mb-5 rounded" data-aos="fade-up">
-                <img src="/images/category-1.png" alt="" className="img-fluid mb-3" />
-                <div className="content text-center p-3">
-                    <div className="title text-center fw-bold fs-2 mb-3">صدقات</div>
+            <div className={`category-card ${borderStyle} pt-5`} data-aos="fade-up">
+                <img src={`/images/${img}`} alt="" className="img-fluid mb-3" />
+                <div className="content text-center pt-4 px-0 px-sm-2 px-xxl-4">
+                    <div className="title text-center fw-bold fs-2 mb-xl-4 mb-3">{title}</div>
                     <div className="verse mb-3">
-                        (إِنَّمَا الصَّدَقَاتُ لِلْفُقَرَاءِ وَالْمَسَاكِينِ وَالْعَامِلِينَ عَلَيْهَا وَالْمُؤَلَّفَةِ قُلُوبُهُمْ وَفِي الرِّقَابِ وَالْغَارِمِينَ وَفِي سَبِيلِ اللَّهِ)    [التوبة:60] 
+                        {description}
                     </div>
-                    <button className='donate-button btn rounded-pill d-block mx-auto px-4' > دفع صدقة </button>
+                    <div className='donate-btn'>
+                        <img src="/images/arrowright.png" alt="" className="img-fluid" />
+                    </div>
                 </div>
             </div>
         </>
