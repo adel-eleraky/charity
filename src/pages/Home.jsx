8455b319635b94557c4f2dfe781328c250ef2a5f
el3raky <kameladel339@gmail.com>
2024-03-20 17:40:33 +0200

    added used & report section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import AssociationCard from '../components/AssociationCard'
 
 function Home() {
     return (
-        <>
+        <div className='home-page'>
             <Header />
             <div className="Campaigns py-5">
                 <div className="container">
@@ -26,40 +26,32 @@ function Home() {
                         <div className="col-12 col-md-6 col-lg-4">
                             <CampaignCard {...{ id: 4, title: "غذاء لسعد كلاكيت تالت مرة", image: "/images/campaign-image.png", description: "وفرلهم احتيجاتهم الاساسية من الغذاء", cost: 1000, paymentMade: 600, moneyRemaining: 400, timeRemaining: 10 }} />
                         </div>
-                        {/* <div className="col-12 col-md-6 col-lg-4">
-                            <CampaignCard />
-                        </div>
-                        <div className="col-12 col-md-6 col-lg-4">
-                            <CampaignCard />
-                        </div>
-                        <div className="col-12 col-md-6 col-lg-4">
-                            <CampaignCard />
-                        </div>
-                        <div className="col-12 col-md-6 col-lg-4">
-                            <CampaignCard />
-                        </div> */}
                     </div>
                 </div>
             </div>
-            <div className="donation_opportunities py-5">
+            <div className="donation_opportunities py-5 position-relative">
+                <img src="/images/donation-opportunity-bg-main.png" alt="" className="img-fluid position-absolute w-100 h-100 top-0" style={{ zIndex: -3 }} />
+                <img src="/images/donation-opportunity-bg-1.png" alt="" className="img-fluid position-absolute bottom-0" />
+                <img src="/images/donation-opportunity-bg-2.png" alt="" className="img-fluid position-absolute bottom-0 start-0" />
                 <div className="container">
-                    <h1 className='text-center mb-5'>فرص التبرع</h1>
-                    <div className="row">
+                    <p className='text-center'>فرص التبرع</p>
+                    <div className="row justify-content-center">
                         <div className="col-12 col-md-6 col-lg-4">
-                            <CategoryCard />
+                            <CategoryCard img="zakat.png" title="زكاة" description=" { وَالْمُقِيمِينَ الصَّلَاةَ ۚ وَالْمُؤْتُونَ الزَّكَاةَ وَالْمُؤْمِنُونَ بِاللَّهِ وَالْيَوْمِ الْآخِرِ أُولَٰئِكَ سَنُؤْتِيهِمْ أَجْرًا عَظِيمًا } [النساء:162]" borderStyle="bottom"/>
                         </div>
                         <div className="col-12 col-md-6 col-lg-4">
-                            <CategoryCard />
+                            <CategoryCard img="sadakat.png" title="صدقات" description="{ إِنَّمَا الصَّدَقَاتُ لِلْفُقَرَاءِ وَالْمَسَاكِينِ وَالْعَامِلِينَ عَلَيْهَا وَالْمُؤَلَّفَةِ قُلُوبُهُمْ وَفِي الرِّقَابِ وَالْغَارِمِينَ وَفِي سَبِيلِ اللَّهِ } [التوبة:60]" borderStyle="bottom"/>
                         </div>
                         <div className="col-12 col-md-6 col-lg-4">
-                            <CategoryCard />
+                            <CategoryCard img="kafarat.png" title="كفارات" description="{ وَالْجُرُوحَ قِصَاصٌ فَمَن تَصَدَّقَ بِهِ فَهُوَ كَفَّارَةٌ لَّهُ ۚ وَمَن لَّمْ يَحْكُم بِمَا أَنزَلَ اللَّهُ فَأُولَـٰئِكَ هُمُ الظَّالِمُونَ } [المائدة:45] " borderStyle="top" />
                         </div>
                         <div className="col-12 col-md-6 col-lg-4">
-                            <CategoryCard />
+                            <CategoryCard img="blood-donation.png" title="تبرع بالدم" description="{ وَمَنْ أَحْيَاهَا فَكَأَنَّمَا أَحْيَا النَّاسَ جَمِيعًا } [المائدة:32]" borderStyle="bottom"/>
                         </div>
                         <div className="col-12 col-md-6 col-lg-4">
-                            <CategoryCard />
+                            <CategoryCard img="used-donation.png" title="التبرعات العينية" description="﴿ وَيُؤثِرونَ عَلى أَنفُسِهِم وَلَو كانَ بِهِم خَصاصَةٌ وَمَن يوقَ شُحَّ نَفسِهِ فَأُولئِكَ هُمُ المُفلِحونَ﴾" borderStyle="top" />
                         </div>
+                        
                     </div>
                 </div>
             </div>
@@ -104,7 +96,24 @@ function Home() {
                     </div>
                 </div>
             </div>
-        </>
+            <div className='container py-5'>
+                <div className="row justify-content-evenly">
+                    <div className="col-12 col-lg-4 report-case text-center pt-4 mb-lg-0 mb-5">
+                        <p className='fs-3 fw-normal'> الابلاغ عن حالة مستحقة </p>
+                        <button className="btn report-btn mb-3 d-block mx-auto"> ابلاغ الان <i className="fa-solid fa-arrow-left me-2"></i> </button>
+                        <img src="/images/report-case.png" alt="" className="img-fluid py-2" />
+                    </div>
+                    <div className="col-12 col-lg-7 used p-4 d-flex justify-content-between">
+                        <div className="content w-50">
+                            <p className='fw-normal fs-2'> ملابس او اجهزة مستعملة </p>
+                            <p style={{ lineHeight: 2 }}> هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات المطابع ودور النشر. كان  </p>
+                            <button className="btn donate-btn"> تبرع الان <i className="fa-solid fa-arrow-left me-2"></i> </button>
+                        </div>
+                        <img src="/images/used-img.png" alt="" className="img-fluid" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
