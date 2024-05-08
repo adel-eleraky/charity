5a85235e69ce14a405960ba2319ba328d7f9741e
el3raky <kameladel339@gmail.com>
2024-05-08 12:22:13 +0300

    created about-us page

---
+++ src/pages/About_us.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import "./css/About_us.css"
+
+function About_us() {
+    return (
+        <div className='about-us-page py-5'>
+            <div className="intro-sec">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <div className='bg-dark text-white text-center fs-2 h-100'> هنا صورة </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <h2 className="sec-title fw-bold mb-5"> التعريف بمؤسسة سبل  </h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus totam possimus illum distinctio esse enim accusamus omnis voluptas! Similique rerum deleniti optio quas facilis ullam minima error cumque eveniet sequi.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="statics-sec">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3"> 
+                            
+                        </div>
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3"></div>
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3"></div>
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About_us
