c7e216a6a7ed854f2a60144171e32d2df859e6de
el3raky <kameladel339@gmail.com>
2024-07-13 01:11:49 +0300

    modified activate account logic

--- src/pages/AccountActivation.jsx
+++ src/pages/AccountActivation.jsx
@@ -7,102 +7,85 @@ import "./css/AccountActivation.css";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as yup from "yup";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
 
 function AccountActivation() {
-  // const [token, setToken] = useState("");
+
+  const [loading, setLoading] = useState(true);
+  const { token } = useParams()
+
+  console.log(token)
   const { user, error, activateStatus } = useSelector(
     (store) => store.userAuth
   );
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // function handleVerify(e) {
-  //   e.preventDefault();
-  //   if (!token) return;
-  //   console.log({ token });
-  //   dispatch(activateAccount(token));
-  // }
 
-  console.log(activateStatus);
-  const initialValues = {
-    token: "",
-  };
 
-  const validationSchema = yup.object().shape({
-    token: yup.string().required("ادخل الرمز"),
-  });
+  // console.log(activateStatus);
+  // const initialValues = {
+  //   token: "",
+  // };
 
-  const submitHandler = (values) => {
-    console.log(values);
-    if (activateStatus === "idle")
-      toast.loading("جارى تأكيد البريد الالكترونى");
-    dispatch(activateAccount(values.token));
-  };
+  // const validationSchema = yup.object().shape({
+  //   token: yup.string().required("ادخل الرمز"),
+  // });
+
+  // const submitHandler = (values) => {
+  //   console.log(values);
+  //   if (activateStatus === "idle")
+  //     toast.loading("جارى تأكيد البريد الالكترونى");
+  //   dispatch(activateAccount(values.token));
+  // };
   // charityauthSlice (login - register are handled in the login page)
   // 3.activate charity account
 
   useEffect(() => {
     toast.dismiss();
 
+    if (activateStatus === "idle" && token) {
+      toast.loading("جارى تأكيد البريد الالكترونى");
+      dispatch(activateAccount(token));
+    }
+
     if (activateStatus === "finished") {
       toast.success("تم تأكيد البريد بنجاح");
-      navigate("/account");
+      setTimeout(() => {
+
+        navigate("/");
+      }, 4000)
     }
+
     if (activateStatus === "failed")
       toast.error("خطأ فى تأكيد البريد الالكترونى");
+
   }, [activateStatus]);
 
   return (
     <div className="account-activation-page py-5 d-flex justify-content-center align-items-center">
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={submitHandler}
-      >
-        {({ values, errors, touched }) => {
-          return (
-            <Form
-              action="post"
-              className="needs-validation p-4 rounded"
-              noValidate
-            >
-              <p className="text-center fw-bold fs-4">
-                {" "}
-                انسخ ارمز المرسل الى بريدك الالكترونى{" "}
-              </p>
-              <div className="input-group has-validation mb-4">
-                <span
-                  className="input-group-text  rounded-0 rounded-end"
-                  id="basic-addon1"
-                >
-                  <i className="fa-solid fa-key"></i>
-                </span>
-                <Field
-                  type="text"
-                  placeholder="ادخل الرمز"
-                  className={`form-control ${
-                    touched.token && errors.token && "is-invalid"
-                  } rounded-0 rounded-start`}
-                  name="token"
-                />
-                <ErrorMessage
-                  name="token"
-                  component="div"
-                  className="invalid-feedback d-block fs-6 fw-bold"
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn d-block w-75 py-2 mt-3 mx-auto fs-4"
-                style={{ backgroundColor: "#927238", color: "white" }}
-              >
-                {" "}
-                تأكيد البريد الاكترونى{" "}
-              </button>
-            </Form>
-          );
-        }}
-      </Formik>
+      <div className="content rounded p-4 fw-bold text-center">
+        {token ?
+
+          <>
+            <h1>تأكيد البريد الالكترونى جارى</h1>
+            <BeatLoader
+              color={"black"}
+              className="d-block mt-3"
+              loading={loading}
+              size={25}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </>
+          :
+          <>
+            <h1 className="mb-3">يجب تأكيد البريد الالكترونى</h1>
+            <h3>تم ارسال رابط الى بريدك الالكترونى </h3>
+          </>
+
+        }
+      </div>
     </div>
   );
 }
