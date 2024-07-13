12edc3bd40df590285ed3920d335f0dd37d71a74
el3raky <kameladel339@gmail.com>
2024-07-13 03:59:18 +0300

    add charity sec to card

--- src/components/CampaignCard.jsx
+++ src/components/CampaignCard.jsx
@@ -5,36 +5,39 @@ import "./css/CampaignCard.css"
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart } from '../rtk/features/CartSlice'
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 
-function CampaignCard({ id, title, img, description, targetDonationAmount, currentDonationAmount }) {
+function CampaignCard({ id, title, img, description, targetDonationAmount, currentDonationAmount, charityImage, charityName }) {
 
     const [donationAmount, setDonationAmount] = useState(10)
     const dispatch = useDispatch()
-    const cart = useSelector((state) => state.casesCart)
+    const navigate = useNavigate()
 
-    const notify = () => {
-        toast.success("تمت اضافة الحالة للسلة", {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-        });
+    const caseProgress = (currentDonationAmount / targetDonationAmount) * 100
+
+    const { userProfile } = useSelector((store) => store.userProfile);
+
+    const handleDonation = () => {
+        if (!userProfile.user) {
+            toast.error("يجب عليك تسجيل الدخول اولا");
+            setTimeout(() => {
+
+                return navigate("/account/login");
+            }, 4000);
+        }
+
     }
     return (
         <>
             <div className="CampaignCard mb-5" data-aos="fade-up" data-aos-duration="1000">
                 <img src={`https://res.cloudinary.com/ddvetozyq/image/upload/v1720813891/caseCoverImages/${img}`} alt="" className="img-fluid w-100 mb-3" />
                 <div className="content p-2">
-                    <div className="title text-center mb-3 "> {title} </div>
+                    <div className="title text-center mb-3 fw-bold"> {title} </div>
                     <div className="description text-center mb-3"> {description} </div>
                     <div className="money_section mb-3">
                         <div className="progress mb-2" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                            <div className="progress-bar" style={{ width: "20%"}}></div>
+                            <div className="progress-bar" style={{ width: `${caseProgress}%` }}></div>
                         </div>
                         <div className="d-flex justify-content-between">
                             <div>
@@ -46,11 +49,19 @@ function CampaignCard({ id, title, img, description, targetDonationAmount, curre
                                 <p className='m-0 fw-bold'> المتبقى </p>
                             </div>
                         </div>
-                        
+
                     </div>
-                    <div className="donation d-flex justify-content-between">
-                        <button className='donate-button btn text-white'>تبرع الان</button>
-                        <button className='addToCart-button btn ' ><i className="bi bi-cart-plus " style={{ color: "#91683A"}}></i> </button>
+                    <div className="donation ">
+                        <div className="input-group mb-3">
+                            <span className="input-group-text  rounded-0 rounded-end" id="basic-addon1"> <i className="fa-solid fa-money-bill-1-wave"></i> </span>
+                            <input type="number" onChange={(e) => setDonationAmount(e.target.value)} className="form-control rounded-0 rounded-start" placeholder="ادخل قيمة التبرع  " />
+                        </div>
+                        <button className='donate-button btn text-white w-100' onClick={handleDonation}>تبرع الان</button>
+                    </div>
+                    <div className="charity-sec d-flex justify-content-between align-items-center mt-3">
+                        <img src={`https://res.cloudinary.com/ddvetozyq/image/upload/v1720813891/charityLogos/${charityImage}`} alt="" className="img-fluid d-block charity-img" />
+                        <p className="charity-name mb-0 mb-0 ms-auto me-2 fw-bold">{charityName}</p>
+                        <button className='btn chat-btn'>  تحدث الان  </button>
                     </div>
                 </div>
             </div>
